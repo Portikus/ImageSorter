52de253e89b467466b5ba5a46d994c782d4f1391
Florian <Portikus@users.noreply.github.com>
2015-09-16 17:39:53 +0000

    Added Universial as External

--- ImageSorter.Tests/Test.cs
+++ ImageSorter.Tests/Test.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using Universial.Test;
 
 namespace ImageSorter.Tests
 {
-    public class Test
+    public class Test : TestBase
     {
         [Test]
         public void Tests()
